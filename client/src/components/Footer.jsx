320d3cceeaff48d8833cffec1b07348159fb43f5
VivekChoudhary15 <b23184@students.iitmandi.ac.in>
2025-06-26 15:24:50 +0530

    Some changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='container px-4 2xl:px-20 mx-auto py-8 flex items-center justify-between gap-4 py-3 mt-2'>
+    <div className='container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-2'>
         <img width={160} src={assets.logo} alt="" />
         <p className='text-gray-400 flex-1 border-1 pl-4 text-sm  max-sm:hidden'>All rights reserved. Copyright @job-portal</p>
         <div className='flex gap-2.5 mt-4'>
